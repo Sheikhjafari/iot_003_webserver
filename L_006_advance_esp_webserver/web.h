54cc5c70f2eb570513961795a4d70e512973425e
sheikhjafari <sheikhjafari@gmail.com>
2020-03-20 20:26:45 +0330

    Update web.h

--- L_006_advance_esp_webserver/web.h
+++ L_006_advance_esp_webserver/web.h
@@ -1,14 +1,20 @@
 const char html_page[] PROGMEM = R"=====(
-
-
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+  <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.0/css/bootstrap-slider.min.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.0/bootstrap-slider.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script src="https://use.fontawesome.com/fd9dba5260.js"></script>
 <style>
 body{
 background: #d3d3d3;
@@ -22,56 +28,13 @@ margin-left : 15px;
  margin-bottom:-5px;
  margin-left : -10px;
  height: 120%;
- width: 120%;
+ width: 100%;
  border: 1px solid #FFFFFF;
  background: #FFFFFF;
 }
 
 
 
-.slidecontainer {
- margin-top : 0px;
- margin-right:0px;
- margin-bottom:0px;
- margin-left : 0px;
-  width: 100%;
-  border: 1px solid #EEEEEE;
-  background: #FFFFFF;
-}
-
-.slider {
-  -webkit-appearance: none;
-  width: 50%;
-  height: 10px;
-  border-radius: 5px;
-  background: #d3d3d3;
-  outline: none;
-  opacity: 0.7;
-  -webkit-transition: .0s;
-  transition: opacity .2s;
-}
-
-.slider:hover {
-  opacity: 1;
-}
-
-.slider::-webkit-slider-thumb {
-  -webkit-appearance: none;
-  appearance: none;
-  width: 25px;
-  height: 25px;
-  border-radius: 50%;
-  background: #326C88;
-  cursor: pointer;
-}
-
-.slider::-moz-range-thumb {
-  width: 25px;
-  height: 25px;
-  border-radius: 50%;
-  background: #326C88;
-  cursor: pointer;
-}
 
 /* Rounded switch */
 
@@ -134,13 +97,92 @@ input:checked + .slider1:before {
   border-radius: 50%;
 }
 
+        .slider-selection {
+        background: #f77500 !important;
+        }
+        .slider-success .slider-selection {
+          background-color: #5cb85c !important;
+        }
+        .slider-primary .slider-selection {
+          background-color: #428bca !important;
+        }
+        .slider-info .slider-selection {
+          background-color: #5bc0de !important;
+        }
+        .slider-warning .slider-selection {
+          background-color: #f0ad4e !important;
+        }
+        .slider-danger .slider-selection {
+          background-color: #d9534f !important;
+        }
+        .slider.slider-horizontal {
+          width: 100% !important;
+          height: 20px;
+        }
+        .slider-handle {
+          background-color: #fff !important;
+          background-image: none !important;
+          -webkit-box-shadow: 1px 1px 24px -2px rgba(0,0,0,0.75) !important;
+          -moz-box-shadow: 1px 1px 24px -2px rgba(0,0,0,0.75) !important;
+          box-shadow: 1px 1px 24px -2px rgba(0,0,0,0.75) !important;
+        }
+
+        .slider-strips .slider-selection {
+          background-image: repeating-linear-gradient(-45deg, transparent, transparent 5px, rgba(255,252,252,0.08) 5px, rgba(252,252,252,0.08) 10px) !important;
+          background-image: -ms-repeating-linear-gradient(-45deg, transparent, transparent 5px, rgba(255,252,252,0.08) 5px, rgba(252,252,252,0.08) 10px) !important;
+          background-image: -o-repeating-linear-gradient(-45deg, transparent, transparent 5px, rgba(255,252,252,0.08) 5px, rgba(252,252,252,0.08) 10px) !important;
+          background-image: -webkit-repeating-linear-gradient(-45deg, transparent, transparent 5px, rgba(255,252,252,0.08) 5px, rgba(252,252,252,0.08) 10px) !important; 
+        }
+        .tooltip-inner {
+          max-width: 200px;
+          padding: 3px 8px;
+          color: #bdbdbd !important;
+          text-align: center;
+          background-color: transparent !important;
+          border-radius: 4px;
+        }
+        .tooltip.top .tooltip-arrow {
+          display: none !important;
+        }
+        .slider .tooltip.top {
+          margin-top: -25px !important;
+        }
+        .well {
+          background: transparent !important;
+          border: none !important;
+          box-shadow: none !important;
+          width: 100% !important;
+          padding: 0;
+        }
+        .slider-ghost .slider-track {
+          height: 5px !important;
+        }
+        .slider-ghost .slider-handle {
+          top: -2px !important;
+          border: 5px solid #f77500;
+        }
+        .slider-success.slider-ghost .slider-handle {
+          border-color: #5cb85c;
+        }
+        .slider-primary.slider-ghost .slider-handle {
+          border-color: #428bca;
+        }
+        .slider-info.slider-ghost .slider-handle {
+          border-color: #5bc0de;
+        }
+        .slider-warning.slider-ghost .slider-handle {
+          border-color: #f0ad4e;
+        }
+        .slider-danger.slider-ghost .slider-handle {
+          border-color: #d9534f;
+        }
 </style>
 </head>
 <body>
 
-<div class="div_header">
+<div class="container-fluid" style="background: #FFFFFF">
 
-<h2 class="h2_header"> ESP8266 Server Control</h2>
+<h2 class="h2_header">ESP8266 Web Control Panel</h2>
 
 </div>
 
@@ -150,18 +192,23 @@ input:checked + .slider1:before {
 
 
 <div class="container-fluid" style="background: #FFFFFF">
-  <div class="row" style="margin-top:10px">
+
+
+
+  <div class="row" style="margin-top:20px ;min-height:50px;">
     <div class="col-sm-8">
       <p>YELLOW LED CONTRAST Value: <span id="slider_p"></span></p>
     </div>
 
     <div class="col-sm-4">
-      <input type="range" min="0" max="1023" value="0" class="slider" id="myRange">
+    <div class="slider-wrapper green">
+      <input class="input-range" id="myRange" data-slider-id='ex1Slider' type="text" data-slider-min="0" data-slider-tooltip="always" data-slider-max="100" data-slider-step="1" data-slider-value="100"/>
+    </div>
     </div>
   </div>
 
 
-  <div class="row">
+  <div class="row" style="min-height:50px;">
     <div class="col-sm-11">
       <p>RED LED Switch: <span id="led_state1">OFF</span></p> 
     </div>
@@ -174,12 +221,12 @@ input:checked + .slider1:before {
     </div>
   </div>
   
-   <div class="row">
+    <div class="row" style="min-height:50px;"> 
     <div class="col-sm-11">
       <p>GREEN LED Switch: <span id="led_state2">OFF</span></p> 
     </div>
 
-    <div class="col-sm-1" style="align-content:flex-start">
+    <div class="col-sm-1">
     <label class="switch">
       <input type="checkbox" onchange="state_change(this,'led_state2')">
       <span class="slider1 round"></span>
@@ -195,16 +242,15 @@ input:checked + .slider1:before {
 var slider = document.getElementById("myRange");
 var output = document.getElementById("slider_p");
   
- i=slider.value;
-   i=Math.round((i*100)/1023);
-  output.innerHTML =i + "%"; 
-  
+ output.innerHTML=100+"%";
+  pwm_change((1023);
 
 slider.onchange = function() {
-   i=this.value;
-   i=Math.round((i*100)/1023);
-  output.innerHTML =i + "%"; 
-  pwm_change(1023-this.value);
+   output.innerHTML=this.value+"%";
+   
+   //i=Math.round((i*100)/1023);
+  //output.innerHTML =i + "%"; 
+ // pwm_change(1023-this.value);
 }
 </script>
 
@@ -242,7 +288,38 @@ function state_change(element,id) {
   xhttp.send();
 }
 </script>
-
+<script>
+(function( $ ){
+$( document ).ready( function() {
+  $( '.input-range').each(function(){
+    var value = $(this).attr('data-slider-value');
+    var separator = value.indexOf(',');
+    if( separator !== -1 ){
+      value = value.split(',');
+      value.forEach(function(item, i, arr) {
+        arr[ i ] = parseFloat( item );
+      });
+    } else {
+      value = parseFloat( value );
+    }
+    $( this ).slider({
+      formatter: function(value) {
+        console.log(value);
+        if(value>0)pwm_change((100-value)*10);
+        return '%' + value;
+      },
+      min: parseFloat( $( this ).attr('data-slider-min') ),
+      max: parseFloat( $( this ).attr('data-slider-max') ), 
+      range: $( this ).attr('data-slider-range'),
+      value: value,
+      tooltip_split: $( this ).attr('data-slider-tooltip_split'),
+      tooltip: $( this ).attr('data-slider-tooltip')
+    });
+  });
+  
+ } );
+} )( jQuery );
+</script>
 </body>
 
 </html>
